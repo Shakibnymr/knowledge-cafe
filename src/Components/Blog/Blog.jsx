dd3a7dde7fd0cd97f957b4aecf9527c9f648336e
shakibnymr <shakibnymr11@gmail.com>
2023-09-17 14:51:27 +0600

    Displayed Blog data and hashtags

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,19 +1,49 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Blog = ({blog}) => {
-    console.log(blog);
-    return (
-        <div>
-            
+const Blog = ({ blog }) => {
+  const {
+    title,
+    cover,
+    author,
+    hashtags,
+    author_img,
+    reading_time,
+    posted_date,
+  } = blog;
+  return (
+    <div className="space-y-4 ">
+      <img
+        className="rounded-lg"
+        src={cover}
+        alt={`cover picture of the title ${title}`}
+      />
+      <div className="flex justify-between">
+        <div className="flex gap-3">
+          <img className="w-14" src={author_img} alt="" />
+          <div>
+            <h2 className="text-2xl">{author}</h2>
+            <p>{posted_date}</p>
+          </div>
         </div>
-    );
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            {" "}
+            <a href="">#{hash}</a>{" "}
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
-
-
-
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
-}
+  blog: PropTypes.object.isRequired,
+};
 
 export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
@@ -13,8 +14,11 @@ useEffect(()=>{
 },[])
 
     return (
-        <div className="md:w-2/3" >
+        <div className="md:w-2/3 space-y-4" >
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
